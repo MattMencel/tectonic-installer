367441d530325358cff6a56993ea766500c51c82
Ethan Devenport <edevenport@users.noreply.github.com>
2017-09-06 10:37:10 -0700

    Use Terraform to bcrypt the adminPassword.
    make docs and examples.
    
    Removed unnecessary opts parameter.
    
    Changed default test password to match the previous encrypted password.
    
    Adjusted requirement wording.
    
    Ran make docs and examples.

--- installer/frontend/ui-tests/utils/awsInstallerInput.js
+++ installer/frontend/ui-tests/utils/awsInstallerInput.js
@@ -7,7 +7,7 @@ const awsTestDatajson = JSON.parse(fs.readFileSync(awsJsonPath, 'utf8'));
 const json = awsTestDatajson.variables;
 /** Returns expected json. This json is used to prep the data required for the test */
 const buildExpectedJson = () => {
-  delete json.tectonic_admin_password_hash;
+  delete json.tectonic_admin_password;
   delete json.tectonic_aws_extra_tags;
   delete json.tectonic_aws_external_private_zone;
   delete json.tectonic_aws_private_endpoints;
